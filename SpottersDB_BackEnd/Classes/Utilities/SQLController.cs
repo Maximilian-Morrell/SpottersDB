1fc24087fceffe24852d5a84381a47ed7803989f
Maximilian Morrell <maximilian.morrell@hak-kitz.ac.at>
2024-09-28 13:15:51 +0200

    Created the SQL Controller Class and programmed methods which connect to the Database & check if it even exists & if not creates it

---
+++ SpottersDB_BackEnd/Classes/Utilities/SQLController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SpottersDB_BackEnd.Classes.Utilities
+{
+    public class SQLController
+    {
+        private SqlConnection con = new SqlConnection("server = (localdb)\\MSSQLLocalDB; integrated security = false;");
+        private SqlCommand cmd = null;
+
+        public void ConnectToDB(string DatabaseName)
+        {
+            try
+            {
+                con.Open();
+                con.ChangeDatabase(DatabaseName);
+                con.Close();
+                con.ConnectionString = "server = (localdb)\\MSSQLLocalDB; integrated security = false; database = " + DatabaseName;
+            }
+            catch (Exception e)
+            {
+                CreateDatabase(DatabaseName);
+            }
+            con.Close();
+        }
+
+        private void CreateDatabase(string DatabaseName)
+        {
+            if(con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.ConnectionString = "server = (localdb)\\MSSQLLocalDB; integrated security = false;";
+            con.Open();
+            cmd = new SqlCommand("CREATE DATABASE " +  DatabaseName, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ConnectToDB(DatabaseName);
+        }
+
+
+    }
+}
--- SpottersDB_BackEnd/Program.cs
+++ SpottersDB_BackEnd/Program.cs
@@ -1,3 +1,5 @@
+using SpottersDB_BackEnd.Classes.Utilities;
+
 namespace SpottersDB_BackEnd
 {
     public class Program
@@ -6,6 +8,9 @@ namespace SpottersDB_BackEnd
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
+            SQLController sqlcontroller = new SQLController();
+            sqlcontroller.ConnectToDB("Maximilian_Morrell_3IT_2024_25");
+
 
             app.MapGet("/", () => "Hello World!");
 
