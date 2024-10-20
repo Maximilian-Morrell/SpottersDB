0a7ca241173692f752974481e63ec4ce4dbcffb9
Maximilian Morrell <maximilian.morrell@hak-kitz.ac.at>
2024-10-20 22:03:49 +0200

    Continuing working

--- SpottersDB_FrontEnd/Classes/Views/EditManufactorerModal.xaml.cs
+++ SpottersDB_FrontEnd/Classes/Views/EditManufactorerModal.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Maui.Platform;
 using SpottersDB_FrontEnd.Classes.Structure;
 using SpottersDB_FrontEnd.Classes.Utilities;
 using System.Collections;
@@ -6,28 +7,34 @@ namespace SpottersDB_FrontEnd.Classes.Views;
 
 public partial class EditManufactorerModal : ContentPage
 {
-    IList<Country> Countries = new List<Country>();
+    public List<Country> Countries = new List<Country>();
     bool IsEditing;
+    int ID = -1;
+    Manufactorer manufactorer;
 
     public EditManufactorerModal()
 	{
 		InitializeComponent();
+        IsEditing = false;
 	}
 
-    public EditManufactorerModal(Manufactorer manufactorer)
+    public EditManufactorerModal(Manufactorer man)
     {
         InitializeComponent();
 
+        manufactorer = man;
         IsEditing = true;
         ManufactorerName.Text = manufactorer.name;
         GetAllRegions();
-        
+
     }
 
     public async void GetAllRegions()
     {
         List<Country> CountriesAll = await HTTP_Controller.GetCountries();
+        Picker test = new Picker();
         List<string> regionNames = new List<string>();
+
         foreach (Country country in CountriesAll)
         {
             if(country.icaO_Code == "")
@@ -37,8 +44,23 @@ public partial class EditManufactorerModal : ContentPage
             }
         }
 
-        
+        test.ItemsSource = regionNames;
 
-       ManufactorerRegionDropDown.ItemsSource = regionNames;
+
+        if (IsEditing)
+        {
+            ID = Countries.FindIndex(c => c.id == manufactorer.region);
+            test.SelectedIndex = ID;
+        }
+
+        test.SelectedIndexChanged += CountrySelectionChanged;
+
+        GridMain.Add(test, 1, 1);
+    }
+
+    private void CountrySelectionChanged(object sender, EventArgs e)
+    {
+        Picker picker = sender as Picker;
+        manufactorer.region = Countries[ID].id;
     }
 }
