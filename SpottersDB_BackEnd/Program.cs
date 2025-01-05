8c1f9ae7b9083a280aa8db5f8ed4d542749f0cd0
Maximilian Morrell <maximilian.morrell@hak-kitz.ac.at>
2025-01-05 16:05:08 +0100

    Country Frontend Validation

--- SpottersDB_BackEnd/Program.cs
+++ SpottersDB_BackEnd/Program.cs
@@ -30,8 +30,6 @@ namespace SpottersDB_BackEnd
             app.UseStaticFiles(StaticOptions);
 
 
-            
-
             app.MapGet("/", () => "Hello World!");
             // Fallback if route is not found
             app.MapFallback(() => Results.NotFound(StatusCodes.Status404NotFound + " - API Route Not Found"));
--- SpottersDB_FrontEnd/Classes/Views/EditCountryModal.xaml.cs
+++ SpottersDB_FrontEnd/Classes/Views/EditCountryModal.xaml.cs
@@ -6,37 +6,57 @@ namespace SpottersDB_FrontEnd.Classes.Views;
 public partial class EditCountryModal : ContentPage
 {
     bool IsEdit;
+    bool IsRegion;
+    bool IsLoaded = false;
     int ID;
+
+    public void InitTxtBoxes()
+    {
+        CountryICAO.Text = "";
+        CountryName.Text = "";
+    }
 
 	public EditCountryModal()
 	{
 		InitializeComponent();
+        InitTxtBoxes();
+        Submit.IsEnabled = false;
         Title = "Create new Country";
         Submit.Text = "Create";
         IsEdit = false;
+        IsRegion = false;
         Submit.Clicked += Submit_Clicked;
+        IsLoaded = true;
     }
 
     public EditCountryModal(bool IsRegionOnly)
     {
         InitializeComponent();
-        Title = "Create new Country";
+        InitTxtBoxes();
+        Title = "Create new Region";
         Submit.Text = "Create";
         IsEdit = false;
+        Submit.IsEnabled = false;
+        IsRegion = IsRegionOnly;
         Submit.Clicked += Submit_Clicked;
-
         CountryICAO.IsEnabled = !IsRegionOnly;
+        IsLoaded = true;
     }
 
     public EditCountryModal(Country c)
     {
+        InitializeComponent();
+        InitTxtBoxes();
         ID = c.id;
         IsEdit = true;
-        InitializeComponent();
         Title = "Edit: " + c.name;
         Submit.Text = "Edit";
-
+        IsRegion = c.icaO_Code.Length == 0;
+        CountryICAO.IsEnabled = !IsRegion;
+        CountryICAO.Text = c.icaO_Code;
+        CountryName.Text = c.name;
         Submit.Clicked += Submit_Clicked;
+        IsLoaded = true;
     }
 
     private async void Submit_Clicked(object sender, EventArgs e)
@@ -55,5 +75,25 @@ public partial class EditCountryModal : ContentPage
             Navigation.RemovePage(this);
         }
 
+    }
+
+
+    private void TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if(IsLoaded)
+        {
+            if (IsRegion)
+            {
+                Submit.IsEnabled = CountryName.Text.Length >= 1;
+            }
+            else if(IsEdit)
+            {
+                Submit.IsEnabled = CountryName.Text.Length >= 1 && CountryICAO.Text.Length >= 1;
+            }
+            else
+            {
+                Submit.IsEnabled = CountryName.Text.Length >= 1;
+            }
+        }
     }
 }
